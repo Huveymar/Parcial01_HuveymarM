d1bd7e547be8e1ef16c99319175babe1248756e8
Huveymar Muñoz <wilsonmunoz2020@itp.edu.co>
2021-03-18 22:51:01 -0500

    correcion de una varable

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ calcularTamaño = re.findall(r'\b[a-z]{1,20}\b', listaPalabras)
  
 for palabra in calcularTamaño:
     contador = frequency.get(palabra,0)
-    frequency[word] = contador + 1
+    frequency[palabra] = contador + 1
      
 repeticion = frequency.keys()
  
 for palabra in repeticion:
-    print (palabra, frequency[words])
+    print (palabra, frequency[palabra])
